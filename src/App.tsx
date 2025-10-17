dee3f0d298779796a0612b1c3dc99d326478fc15
José Vitor <jose-vitor-7@hotmail.com>
2025-10-17 14:08:17 -0300

    feat: write & read file (temp db)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { toast, Toaster } from 'sonner'
 import { v4 as uuidv4 } from 'uuid'
+import { Check, Loader2, Save, Trash, Undo } from 'lucide-react'
+import clsx from 'clsx'
+import { open, exists, mkdir } from '@tauri-apps/plugin-fs';
+import * as path from '@tauri-apps/api/path';
 
 import './index.css'
-import { Check, Trash, Undo } from 'lucide-react'
-import clsx from 'clsx'
 
 
 interface ITodo {
@@ -19,8 +21,9 @@ interface ITodo {
 
 export const App = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
-  
   const [title, setTitle] = useState('')
+  
+  const [saveStatus, setSaveStatus] = useState<'idle' | 'unsaved' | 'saving' | 'saved' | 'error'>('idle')
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value)
@@ -43,7 +46,7 @@ export const App = () => {
 
       setTitle('')
 
-      toast.success('Todo added successfully')
+      setSaveStatus('unsaved')
     } catch (error) {
       toast.error('Failed to add todo', {
         description: error instanceof Error ? error.message : String(error),
@@ -53,16 +56,98 @@ export const App = () => {
 
   const handleCompleteTodo = (id: string) => {
     setTodos((prevTodos) => prevTodos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+    setSaveStatus('unsaved')
   }
 
   const handleDeleteTodo = (id: string) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+    setSaveStatus('unsaved')
   }
+
+  const handleLoadTodos = async () => {
+    try {
+      const appDataDir = await path.appDataDir()
+      const appDir = await path.join(appDataDir, 'tauri-todo')
+      const filePath = await path.join(appDir, 'todos.json')
+
+      const appDirExists = await exists(appDir)
+
+      if (!appDirExists) {
+        await mkdir(appDir, { recursive: true })
+      }
+
+      const filePathExists = await exists(filePath)
+
+      if (filePathExists) {
+        const file = await open(filePath, { read: true })
+        const stat = await file.stat()
+        const buffer = new Uint8Array(stat.size)
+        await file.read(buffer)
+  
+        const content = new TextDecoder().decode(buffer)
+        const todos = JSON.parse(content) as ITodo[]
+        
+        await file.close()
+  
+        setTodos(todos)
+      }
+    } catch (error) {
+      toast.error('Failed to load todos', {
+        description: error instanceof Error ? error.message : String(error),
+      })
+    }
+  }
+
+  const handleSaveTodos = async () => {
+    try {
+      setSaveStatus('saving')
+
+      const appDataDir = await path.appDataDir()
+      const appDir = await path.join(appDataDir, 'tauri-todo')
+      const filePath = await path.join(appDir, 'todos.json')
+
+      const appDirExists = await exists(appDir)
+
+      if (!appDirExists) {
+        await mkdir(appDir, { recursive: true })
+      }
+
+      const file = await open(filePath, { write: true, create: true })
+      await file.write(new TextEncoder().encode(JSON.stringify(todos)))
+      await file.close()
+
+      setSaveStatus('saved')
+      
+      toast.success('Todos saved successfully')
+    } catch (error) {
+      toast.error('Failed to save todos', {
+        description: error instanceof Error ? error.message : String(error),
+      })
+
+      setSaveStatus('error')
+    }
+  }
+
+  useEffect(() => {
+    handleLoadTodos()
+  }, [])
 
   return (
     <div className="w-screen h-screen bg-zinc-50 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50">
       <Toaster richColors />
-      <div className="max-w-[1200px] mx-auto">
+      <div className="max-w-[1200px] mx-auto w-[90%] py-10">
+        <div className="flex justify-end items-center gap-2">
+          {saveStatus === 'unsaved' && (<span className="text-xs">Não salvo</span>)}
+          {saveStatus === 'saving' && (<span className="text-xs">Salvando...</span>)}
+          {saveStatus === 'saved' && (<span className="text-xs">Salvo</span>)}
+          {saveStatus === 'error' && (<span className="text-xs">Erro ao salvar</span>)}
+          <button
+            className="bg-zinc-100 dark:bg-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-800 text-zinc-950 dark:text-zinc-50 rounded-lg p-2 transition-all cursor-pointer"
+            onClick={handleSaveTodos}
+          >
+            {saveStatus === 'saving' ? <Loader2 className="w-4 h-4 animate-spin" /> : <Save className="w-4 h-4" />}
+          </button>
+        </div>
         <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
           <label htmlFor="title">Title</label>
           <input
@@ -86,8 +171,7 @@ export const App = () => {
               className={
                 clsx(
                   'group flex items-center gap-2 justify-between px-4 py-2 rounded-lg',
-                  todo.completed ? 'bg-emerald-500 opacity-70' : 'bg-zinc-900',
-
+                  todo.completed ? 'bg-emerald-500 opacity-70' : 'bg-zinc-100 dark:bg-zinc-900 text-zinc-950 dark:text-zinc-50',
                 )
               }
             >
